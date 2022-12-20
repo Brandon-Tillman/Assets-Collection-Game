5cf55a156ee5dce23912a8b03921757f804356db
Brandon-Tillman <70339232+Brandon-Tillman@users.noreply.github.com>
2022-12-20 00:50:21 -0600

    Add files via upload

--- Scripts/Camera2DFollow.cs
+++ Scripts/Camera2DFollow.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEngine;
 
+// this comes from the Unity Standard Assets
 namespace UnityStandardAssets._2D
 {
     public class Camera2DFollow : MonoBehaviour
@@ -11,10 +12,11 @@ namespace UnityStandardAssets._2D
         public float lookAheadReturnSpeed = 0.5f;
         public float lookAheadMoveThreshold = 0.1f;
 
-        private float m_OffsetZ;
-        private Vector3 m_LastTargetPosition;
-        private Vector3 m_CurrentVelocity;
-        private Vector3 m_LookAheadPos;
+		// private variables
+        float m_OffsetZ;
+        Vector3 m_LastTargetPosition;
+        Vector3 m_CurrentVelocity;
+        Vector3 m_LookAheadPos;
 
         // Use this for initialization
         private void Start()
@@ -22,12 +24,23 @@ namespace UnityStandardAssets._2D
             m_LastTargetPosition = target.position;
             m_OffsetZ = (transform.position - target.position).z;
             transform.parent = null;
+
+			// if target not set, then set it to the player
+			if (target==null) {
+				target = GameObject.FindGameObjectWithTag("Player").transform;
+			}
+
+			if (target==null)
+				Debug.LogError("Target not set on Camera2DFollow.");
+
         }
 
-
         // Update is called once per frame
-        private void Update()
+		private void Update()
         {
+			if (target == null)
+				return;
+
             // only update lookahead pos if accelerating or changed direction
             float xMoveDelta = (target.position - m_LastTargetPosition).x;
 
@@ -39,7 +52,7 @@ namespace UnityStandardAssets._2D
             }
             else
             {
-                m_LookAheadPos = Vector3.MoveTowards(m_LookAheadPos, Vector3.zero, Time.deltaTime*lookAheadReturnSpeed);
+				m_LookAheadPos = Vector3.MoveTowards(m_LookAheadPos, Vector3.zero, Time.deltaTime*lookAheadReturnSpeed);
             }
 
             Vector3 aheadTargetPos = target.position + m_LookAheadPos + Vector3.forward*m_OffsetZ;
